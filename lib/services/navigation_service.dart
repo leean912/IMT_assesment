89477fde3ca6ee6e3530bf2b06034e39ffb43c36
lee.an <lee.an@cognixus.com>
2024-01-25 09:02:15 +0800

    third commit

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -1,3 +1,39 @@
+<<<<<<< HEAD
 routSetting() {
   
+=======
+import 'package:flutter/material.dart';
+import 'package:flutter_profile_demo/screens/favourite_profile_page.dart';
+import 'package:flutter_profile_demo/screens/home.dart';
+import 'package:flutter_profile_demo/screens/profile_details_page.dart';
+
+routSetting(RouteSettings settings) {
+  final String? routeName = settings.name;
+
+        if (routeName == MyHomePage.myHomePageroute) {
+          return MaterialPageRoute(
+            builder: (BuildContext context) {
+              return const MyHomePage();
+            },
+          );
+        }
+
+        if (routeName == ProfileDetailsPage.profileDetailsPageRoute) {
+          return MaterialPageRoute(
+            builder: (BuildContext context) {
+              return const ProfileDetailsPage();
+            },
+          );
+        }
+
+        if (routeName == FavouriteProfilePage.favouriteProfilePageRoute) {
+          return MaterialPageRoute(
+            builder: (BuildContext context) {
+              return const FavouriteProfilePage();
+            },
+          );
+        }
+        
+        return null;
+>>>>>>> 5d39302 (second commit)
 }
